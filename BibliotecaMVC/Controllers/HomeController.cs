18c0de186cff5b8b557303094df8a9a24e86f6f0
dcanjura <diegoac053@gmail.com>
2026-08-13 19:07:06 -0600

    Cambios de semana 4 aplicados

--- BibliotecaMVC/Controllers/HomeController.cs
+++ BibliotecaMVC/Controllers/HomeController.cs
@@ -23,11 +23,6 @@ namespace BibliotecaMVC.Controllers
             return View();
         }
 
-        public IActionResult Autores()
-        {
-            return View();
-        }
-
         public IActionResult Categorias()
         {
             return View();
